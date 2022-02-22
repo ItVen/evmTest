c1a2a7a6ccb3c8f8ceb11912ee55fc59dc22c0c2
ItVen <751733381@qq.com>
2022-02-22 19:10:41 +0800

    update quick add key

--- src/unipass/tx/addLocalKey.js
+++ src/unipass/tx/addLocalKey.js
@@ -21,7 +21,7 @@ const nonce = argsUsername[1];
 
 async function getAddLocalKeyTxData() {
   const account = getFileData(`./mock/${fileName}.json`, true);
-  const rsaKey = getRSAData();
+  const rsaKey = await getRSAData();
 
   const inner = {
     chainId: process.env.CHAIN_ID,
